13799f2910df8a2bf13c61156e637a3e2eb64b76
eugene.tishkevich <eugene.tishkevich@aiolane.com>
2020-11-05 15:56:46 +0300

    refactoring

--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,26 @@ module.exports = {
   output: {
     path: __dirname + '/lib',
     filename: 'index.js',
+    library: 'decentr',
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+    globalObject: 'typeof self !== \'undefined\' ? self : this',
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
   },
   module: {
     rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
       {
         test: /\.tsx?$/,
         use: 'ts-loader',
