5ca638a47cef08d66805a0a9c3c8167a46b39269
Eugene Tishkevich <eugene.tishkevich@gmail.com>
2022-05-24 17:22:53 +0300

    feature: sentinel: update message urls

--- src/api/blockchain/sentinel/subscription/client.ts
+++ src/api/blockchain/sentinel/subscription/client.ts
@@ -61,7 +61,7 @@ export class SubscriptionClient {
     },
   ): TransactionSigner {
     const message = createTypedEncodeObject(
-      TxMessageTypeUrl.SubscribeToNode,
+      TxMessageTypeUrl.SubscriptionSubscribeToNode,
       request,
     );
 
